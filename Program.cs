043aa77499886d0053db0005c487c584317457f9
Pablo Cardoso <pabloluiz30@gmail.com>
2024-09-09 10:47:56 -0300

    Desafio concluidogit add Program.cs!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Ligar Nokia");
+Nokia nokia = new Nokia("123456", "Tijolão", "56789", 64);
+nokia.Ligar();
+Console.WriteLine($"Nokia está ligado");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\nLigar IPhone");
+Iphone iphone = new Iphone("987654", "iPhone 13 Pro Max", "09876", 64);
+iphone.Ligar();
+Console.WriteLine("Iphone está ligado");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
